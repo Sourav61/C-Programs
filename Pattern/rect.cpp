bfa222b491112e74e484585e1642b8b4daf8b538
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-09-02 01:21:14 +0530

    Create rect.cpp

---
+++ Pattern/rect.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int row,col,j;
+    cout<<"Enter the no. of rows"<<endl;
+    cin>>row;
+    cout<<"Enter the no. of columns"<<endl;
+    cin>>col;
+    for(int i=1;i<=row;i++){
+        for(int j=1;j<=col;j++){
+            cout<<'*';
+        }
+        cout<<endl;
+    }
+}
