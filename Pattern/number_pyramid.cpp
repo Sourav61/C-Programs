8af1994f05d0e4ebef4158554f2b77725957ce6a
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-09-02 08:41:42 +0530

    Create number_pyramid.cpp

---
+++ Pattern/number_pyramid.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n,i,j,k;
+    cout<<"Enter the value of n"<<endl;
+    cin>>n;
+
+    for(i=n;i>1;i--){
+        for(j=1;j<i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    for(i=1;i<=n;i++){
+        for(j=1;j<i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    
+    return 0;
+}
