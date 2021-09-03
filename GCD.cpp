3330b8a436c438129d7703854c4a6b48c7aebc42
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-09-03 11:19:43 +0530

    Create GCD.cpp

---
+++ GCD.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,i=0,res=0;
+    cout<<"Enter the first number"<<endl;
+    cin>>a;
+
+    cout<<"Enter the second number"<<endl;
+    cin>>b;
+
+    for(i=2;i<=a+b;i++){
+        if(a%i==0 && b%i==0){
+            res=i;
+        }
+    }
+    cout<<res;
+}
