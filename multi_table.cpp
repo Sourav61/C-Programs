56b4072932e3dab6d076ab8199d1fcfd65124d69
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-09-04 13:02:59 +0530

    Update and rename mult_table.cpp to multi_table.cpp

--- multi_table.cpp
+++ multi_table.cpp
@@ -6,15 +6,15 @@ int main(){
     cout<<"Input the number upto: "<<endl;
     cin>>n;
 
-    cout<<"Multiplication table from 0 to "<<n<<" is: ";
+    cout<<"Multiplication table from 0 to "<<n<<" is: "<<endl;
     for (int i = 1; i <= 10; i++) 
     {
         for (int j = 1; j <= n; j++) 
         {
             if (j <= n - 1)
-                cout << j << "x" << i << "=" << i * j<<"  ";
+                cout << j << "x" << i << "=" << i * j<<"\t\t";
             else
-                cout << j << "x" << i << "=" <<i * j<<" ";
+                cout << j << "x" << i << "=" <<i * j<<"\t";
         }
         cout << endl;
     }
