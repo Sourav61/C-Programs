a8b05be594157f383aabfc7d839c5cd83b781542
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-09-06 00:34:27 +0530

    Create armstrong.cpp

---
+++ armstrong.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main(){
+    int n,sum=0;
+    cout<<"Enter the number to check whether it is an Armstrong number or not: "<<endl;
+    cin>>n;
+    int a=n;
+
+    while(n!=0){
+        sum+=pow((n%10),3);
+        n = n/10;
+    }
+
+    if(sum==a){
+        cout<<"The entered number "<<a<<" is an Armstrong number!!"<<endl; 
+    }
+    else{
+        cout<<"The entered number "<<a<<" is not an Armstrong number!!"<<endl;
+    }
+
+    return 0;
+}
