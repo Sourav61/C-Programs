35b275f7428a24e4561602d81124e4087bd12997
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-09-06 01:37:39 +0530

    Update prime.cpp

--- prime.cpp
+++ prime.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 
 int main(){
@@ -7,13 +8,34 @@ int main(){
     cout<<"Enter a number"<<endl;
     cin>>n;
 
-    for(i=2;i<n;i++){
+    // Method-1(Self)
+
+    // for(i=2;i<n;i++){
+    //     if(n%i==0){
+    //         cout<<"The entered number "<<n<<" is not prime number as it is divisible by "<<i<<endl;
+    //         break;
+    //     }
+    // }
+    // if(i==n){
+    // cout<<"The entered number "<<n<<" is a prime number"<<endl;
+    // }
+
+    // --------------------------------------------------------------------------------------------------
+
+    // Method-2
+
+    bool flag=0;
+
+    for(int i=2;i<=sqrt(n);i++){
         if(n%i==0){
-            cout<<"The entered number "<<n<<" is not prime number as it is divisible by "<<i<<endl;
+            cout<<"\nThe entered number "<<n<<" is not a prime number!!"<<endl;
+            flag=1;
             break;
         }
+        if(flag==0){
+            cout<<"\nThe entered number "<<n<<" is a prime number!!"<<endl;
+        }
     }
-    if(i==n){
-    cout<<"The entered number "<<n<<" is a prime number"<<endl;
-    }
+
+    return 0;
 }
