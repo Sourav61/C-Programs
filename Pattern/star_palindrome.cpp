e402b0046aa49ed64bf755f318bf3418bcc087e4
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-09-05 11:09:06 +0530

    Update star_palindrome.cpp

--- Pattern/star_palindrome.cpp
+++ Pattern/star_palindrome.cpp
@@ -6,34 +6,60 @@ int main(){
     cout<<"Enter the number of rows of star palindromic pattern: "<<endl;
     cin>>n;
 
+    // Method-1(Self)
+
+    // for(int i=1;i<=n;i++){
+    //     int j;
+    //     for(j=1;j<=n-i;j++){
+    //         cout<<"   ";
+    //     }
+    //     for(;j<=n;j++){
+    //         cout<<" * ";
+    //     }
+    //     // k=2;
+    //     for(;j<=n+i-1;j++){
+    //         cout<<" * ";
+    //     }
+    //     cout<<endl;
+
+    // }
+        
+    // for(int i=n;i>0;i--){
+
+    //     int j;
+    //     for(j=n-i;j>0;j--){
+    //         cout<<"   ";
+    //     }
+    //     for(;j<=i-1;j++){
+    //         cout<<" * ";
+    //     }
+
+    //     for(;j>1;j--){
+    //         cout<<" * ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // -----------------------------------------------------------------------
+
+    //Method-2
+
     for(int i=1;i<=n;i++){
-        int j;
-        for(j=1;j<=n-i;j++){
-            cout<<"   ";
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
         }
-        for(;j<=n;j++){
-            cout<<" * ";
-        }
-        // k=2;
-        for(;j<=n+i-1;j++){
-            cout<<" * ";
+        for(int j=1;j<=2*i-1;j++){
+            cout<<" *";
         }
         cout<<endl;
-
     }
-        
+
     for(int i=n;i>0;i--){
-
-        int j;
-        for(j=n-i;j>0;j--){
-            cout<<"   ";
+        for(int j=1;j<=n-i;j++){
+            cout<<"  ";
         }
-        for(;j<=i-1;j++){
-            cout<<" * ";
-        }
-
-        for(;j>1;j--){
-            cout<<" * ";
+        for(int j=1;j<=2*i-1;j++){
+            cout<<" *";
         }
         cout<<endl;
     }
