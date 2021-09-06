e3bbf30cb65414aa1662a8589857dc73e4a99f9b
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-09-06 10:46:44 +0530

    Create ascii.cpp

---
+++ ascii.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    char c;
+    cout<<"Enter any character to print it's ASCII value: "<<endl; 
+    cin>>c;
+
+    cout<<"The ASCII value of given character "<<c<<" is: "<<int(c); 
+}
