d25ebb05bc72d5f1cd95ff6f59168bdd92b819b6
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-09-14 22:40:40 +0530

    Update func_pyth_triplet.cpp

--- func_pyth_triplet.cpp
+++ func_pyth_triplet.cpp
@@ -1,20 +1,42 @@
 #include<iostream>
 using namespace std;
 
-int check(int a,int b,int c){
-    if((a*a) == (b*b)+(c*c) || (b*b) == (a*a)+(c*c) || (c*c) == (a*a)+(b*b)){
-        return 1;
+// int check(int a,int b,int c){
+//     if((a*a) == (b*b)+(c*c) || (b*b) == (a*a)+(c*c) || (c*c) == (a*a)+(b*b)){
+//         return 1;
+//     }else{
+//         return 0;
+//     }
+// }
+
+bool check(int a,int b,int c){
+    int x = max(a,max(b,c));
+    int y,z;
+
+    if(x==a){
+        y=b;
+        z=c;
+    }else if(x == b){
+        y=a;
+        z=c;
     }else{
-        return 0;
+        y=a;
+        z=b;
     }
+
+    if(x*x == y*y+z*z)
+        return true;
+    else
+        return false;
 }
 
 int main(){
     int a,b,c;
+    cout<<"Enter the three numbers to check for a pythagorean triplet"<<endl;
     cin>>a>>b>>c;
 
     if(check(a,b,c)){
-        cout<<"The numbers :"<<a<<", "<<b<<" and "<<c<<" together make a Pythagorean triplet";
+        cout<<"The nummbers :"<<a<<", "<<b<<" and "<<c<<" together make a Pythagorean triplet";
     }
     else{
         cout<<"The numbers :"<<a<<", "<<b<<" and "<<c<<" together do not make a Pythagorean triplet";
