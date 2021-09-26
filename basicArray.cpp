323ced9ee3e818df39f8a7e98186bd4f7109ad02
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-09-26 15:16:47 +0530

    Create basicArray.cpp

---
+++ basicArray.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in array " << endl;
+    cin >> n;
+    int arr[n], i;
+
+    for (i = 1; i <= n; i++)
+    {
+        cout << "Enter the element of the array at " << i << " position: " << endl;
+        cin >> arr[i];
+    }
+
+    cout << "The array obtained is: " << endl;
+
+    for (i = 1; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
