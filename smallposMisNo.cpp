48cc5dbd3bc61c1cac80797c6f8ac0b4cc0be230
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-10-16 21:37:06 +0530

    Create smallposMisNo.cpp

---
+++ smallposMisNo.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the total number of elements in an array: " << endl;
+    cin >> n;
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the element at " << i << " position of the array: " << endl;
+        cin >> a[i];
+    }
+
+    int count = 1;
+
+    while (count < n)
+    {
+        for (int i = 0; i < n - count; i++)
+        {
+            if (a[i] > a[i + 1])
+            {
+                int temp = a[i + 1];
+                a[i + 1] = a[i];
+                a[i] = temp;
+            }
+        }
+        count++;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+
+        // cout << i << " ";
+        if (a[i] != i)
+        {
+            cout << i << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
