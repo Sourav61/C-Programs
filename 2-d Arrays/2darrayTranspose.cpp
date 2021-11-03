06519fd5c2859b2d134d60a0d6d78ef8100cc621
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-11-03 20:40:55 +0530

    Create 2darrayTranspose.cpp

---
+++ 2-d Arrays/2darrayTranspose.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of rows and columns of square matrix: " << endl;
+    cin >> n;
+
+    int a[n][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "Enter the element at " << i << " row and " << j << " column in square matrix: " << endl;
+            cin >> a[i][j];
+        }
+    }
+
+    cout << "The original array is: " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            // My method
+            // Swapping using any operator
+            if (i != j)
+            {
+                a[i][j] = a[i][j] + a[j][i];
+                a[j][i] = a[i][j] - a[j][i];
+                a[i][j] = a[i][j] - a[j][i];
+            }
+
+        }
+    }
+
+    cout << "The square matrix obtained after transpose is: " << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << a[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
