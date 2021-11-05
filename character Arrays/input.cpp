ac5d68f35a279fd8e3d5d044b2747978ef0efd34
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-11-05 22:41:59 +0530

    Added sizeof() operator

--- character Arrays/input.cpp
+++ character Arrays/input.cpp
@@ -7,7 +7,11 @@ int main()
     cout << "Enter the character array: " << endl;
     cin >> arr;
 
-    cout << arr[1] << " ";
+    cout << arr[1] << endl;
+
+    // sizeof() operator to check size of the character!
+
+    cout << sizeof(arr[1]) << endl;
 
     return 0;
 }
