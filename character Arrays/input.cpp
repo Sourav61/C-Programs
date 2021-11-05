35a89db05fc07509fa86f579fedfa1d43359f33a
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-11-05 22:38:17 +0530

    Create input.cpp

---
+++ character Arrays/input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char arr[100];
+    cout << "Enter the character array: " << endl;
+    cin >> arr;
+
+    cout << arr[1] << " ";
+
+    return 0;
+}
