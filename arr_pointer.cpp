098823e63330a3618902856651ea719a1eb59427
Sourav Pahwa <70853906+Sourav61@users.noreply.github.com>
2021-11-15 00:06:11 +0530

    Added User Input

--- arr_pointer.cpp
+++ arr_pointer.cpp
@@ -3,17 +3,29 @@ using namespace std;
 
 int main()
 {
-    int arr[] = {10, 20, 30};
+    int n;
+    cout << "Enter the length of the array: " << endl;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the element at " << i << " position of the array: " << endl;
+        cin >> arr[i];
+    }
+
+    cout << "The array is: " << endl;
+
     // cout << *arr << endl;
 
     int *ptr = arr;
-    // for (int i = 0; i < 3; i++)
+    // for (int i = 0; i < n; i++)
     // {
     //     cout << *ptr << endl;
     //     ptr++;
     // }
 
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < n; i++)
     {
         cout << *(arr + i) << endl;
     }
