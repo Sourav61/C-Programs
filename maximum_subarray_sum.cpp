402b0d75c575928057125c07c217235adbc1e27f
sourav pahwa <Sourav61pahwa@gmail.com>
2022-04-15 23:47:48 +0530

    maximum sub array
    added different approaches for maximum sub array

--- maximum_subarray_sum.cpp
+++ maximum_subarray_sum.cpp
@@ -34,4 +34,52 @@ int main()
     // cout << "The maximum sum of any subarray in the given array is: " << maxSum << endl;
 
     // Optimized Approach ==========================================================
+
+    // My Approach
+
+    // int i = 0, j = 0, sum = 0;
+
+    // int maxSum = INT_MIN;
+
+    // for (int i = 0; i < n; i++)
+    // {
+    //     int sum = 0;
+    //     for (int j = i; j < n; j++)
+    //     {
+    //         sum += a[j];
+    //         maxSum = max(maxSum, sum);
+    //     }
+    // }
+
+    // cout << "The maximum sum out of all subarrays in the given array is: " << maxSum << endl;
+
+    // Approach in Lecture 8.6 ========================================================================================
+
+    int currSum[n + 1];
+    currSum[0] = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        currSum[i] = currSum[i - 1] + a[i - 1];
+    }
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << currSum[i] << " ";
+    }
+    cout << endl;
+
+    int maxSum = INT_MIN;
+
+    for (int i = 1; i <= n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < i; j++)
+        {
+            sum = currSum[i] - currSum[j];
+            maxSum = max(sum, maxSum);
+        }
+    }
+
+    cout << maxSum << endl;
 }
