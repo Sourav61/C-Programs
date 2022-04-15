275be8fce6cd20b9e5f7904490fbcb5d5e840f55
sourav pahwa <Sourav61pahwa@gmail.com>
2022-04-15 21:28:17 +0530

    print all subarrays
    added print all subarrays program

---
+++ printAllSubArrays.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in the array: " << endl;
+    cin >> n;
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the element at the position " << i << " in the array: " << endl;
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << a[k] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+}
