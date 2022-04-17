61beece2f3bc151290c4269fe6bb450410a79ecd
sourav pahwa <Sourav61pahwa@gmail.com>
2022-04-17 20:13:39 +0530

    Basics of String
    Lecture 31: Basics of String

---
+++ strings/basics.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Different ways of declaring a string
+
+    // string str;
+    // cout << "Please enter any word: " << endl;
+    // cin >> str;
+    // cout << "Word entered by you is: " << str;
+
+    // --------------------------------------------------
+
+    // string str1(5, 'S');
+
+    // cout << str1 << endl;
+
+    // --------------------------------------------------
+
+    // string str2 = "Word s";
+
+    // cout << str2 << endl;
+
+    // =============================================================================
+    // To input a whole sentence
+
+    // string sp;
+
+    // cin(getline, sp);
+
+    // cout << sp << endl;
+
+    // Combining two strings using various methods
+
+    // string str;
+
+    string s1 = "fam";
+    string s2 = "ily";
+
+    // s1.append(s2);
+
+    // cout << s1 << endl;
+
+    // cout << s1 + s2 << endl;
+
+    // s1 = s1 + s2;
+
+    // cout << s1 << endl;
+
+    cout << s1[1] << endl;
+
+    return 0;
+}
