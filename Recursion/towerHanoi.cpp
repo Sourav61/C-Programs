0c19f75fbbfd8eb5ec50d6c8a46d492b36108cb3
sourav pahwa <Sourav61pahwa@gmail.com>
2022-04-28 07:34:00 +0530

    towerOfHanoi
    towerOfHanoi

---
+++ Recursion/towerHanoi.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void towerOfHanoi(int n, char src, char dest, char helper)
+{
+    if (n == 0)
+    {
+        return;
+    }
+    towerOfHanoi(n - 1, src, helper, dest);
+    cout << "Move from " << src << " to " << dest << endl;
+    towerOfHanoi(n - 1, helper, src, dest);
+}
+
+int main()
+{
+    int n;
+    char src, helper, dest;
+
+    cout << "Please enter any number: " << endl;
+    cin >> n;
+
+    cout << "Please enter the source, helper and destination node: " << endl;
+    cin >> src >> helper >> dest;
+
+    cout << "The steps to be followed in transfer between source node " << src << " and destination node " << dest << " using helper node " << helper << " is: " << endl;
+    towerOfHanoi(n, src, dest, helper);
+
+    return 0;
+}
