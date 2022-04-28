114423a54e2ecab71e79310c02cd528be134ccae
sourav pahwa <Sourav61pahwa@gmail.com>
2022-04-28 19:24:18 +0530

    covered till 4th problem video 40/226
    covered till 4th problem video 40/226

---
+++ Recursion/removeDup.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+string removeDup(string s)
+{
+    if (s.length() == 0)
+    {
+        return "";
+    }
+
+    char ch = s[0];
+
+    string ans = removeDup(s.substr(1));
+
+    if (ch == ans[0])
+    {
+        return ans;
+    }
+
+    return ch + ans;
+}
+
+int main()
+{
+    string s;
+    cout << "Please enter any word: " << endl;
+
+    cin >> s;
+
+    cout << "After removing duplicates from given string " << s << ", the string becomes: " << removeDup(s) << endl;
+}
--- Recursion/towerHanoi.cpp
+++ Recursion/towerHanoi.cpp
@@ -9,7 +9,7 @@ void towerOfHanoi(int n, char src, char dest, char helper)
     }
     towerOfHanoi(n - 1, src, helper, dest);
     cout << "Move from " << src << " to " << dest << endl;
-    towerOfHanoi(n - 1, helper, src, dest);
+    towerOfHanoi(n - 1, helper, dest, src);
 }
 
 int main()
