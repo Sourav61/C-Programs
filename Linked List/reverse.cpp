acf072667596db1996f24eeb667c962d2b39c1c2
sourav pahwa <Sourav61pahwa@gmail.com>
2022-05-13 08:04:02 +0530

    Covered Till Video Lecture 22.3
    Covered Till Video Lecture 22.3

---
+++ Linked List/reverse.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    node *next;
+    int data;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+
+    n->next = head;
+
+    head = n;
+}
+
+void deletionAtHead(node *&head)
+{
+    node *todelete = head;
+
+    head = head->next;
+
+    delete todelete;
+}
+
+void insertAtTail(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+}
+
+void deletion(node *&head, int val)
+{
+    if (head == NULL)
+    {
+        return;
+    }
+
+    if (head->next = NULL)
+    {
+        deletionAtHead(head);
+        return;
+    }
+
+    node *temp = head;
+
+    while (temp->next->data != val)
+    {
+        temp = temp->next;
+    }
+
+    node *todelete = temp->next;
+
+    temp->next = temp->next->next;
+
+    delete todelete;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+node *reverse(node *&head)
+{
+    node *prev = NULL;
+    node *curr = head;
+    node *next;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+
+        prev = curr;
+        curr = next;
+    }
+
+    return prev;
+}
+
+node *reverseRecursive(node *&head)
+{
+
+    if (head == NULL || head->next == NULL)
+    {
+        return head;
+    }
+
+    node *newhead = reverseRecursive(head->next);
+
+    head->next->next = head;
+
+    head->next = NULL;
+
+    return newhead;
+}
+
+int main()
+{
+    node *head = NULL;
+
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    display(head);
+
+    // reverse iteratively
+    node *newhead = reverse(head);
+
+    // reverse recursively
+    node *newhead = reverseRecursive(head);
+
+    display(newhead);
+
+    return 0;
+}
