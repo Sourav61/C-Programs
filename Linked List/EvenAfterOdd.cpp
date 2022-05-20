0b568994bded0381e2fd95f4adca7002baed143d
sourav pahwa <Sourav61pahwa@gmail.com>
2022-05-20 20:39:27 +0530

    Covered till video lecture 22.11
    Covered till video lecture 22.11

---
+++ Linked List/EvenAfterOdd.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+
+    n->next = head;
+
+    head = n;
+}
+
+void insertAtTail(node *&head, int val)
+{
+    if (head == NULL)
+    {
+        insertAtHead(head, val);
+        return;
+    }
+
+    node *n = new node(val);
+    node *temp = head;
+
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = n;
+}
+
+void evenAfterOdd(node *&head)
+{
+    node *odd = head;
+    node *even = head->next;
+
+    node *evenStart = even;
+
+    while (odd->next != NULL && even->next != NULL)
+    {
+        odd->next = even->next;
+        odd = odd->next;
+
+        even->next = odd->next;
+        even = even->next;
+    }
+
+    odd->next = evenStart;
+
+    if (odd->next != NULL)
+    {
+        even->next = NULL;
+    }
+}
+
+void display(node *head)
+{
+    node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+
+    int a[] = {1, 2, 3, 4, 5, 6};
+
+    for (int i = 0; i < 6; i++)
+    {
+        insertAtTail(head, a[i]);
+    }
+
+    display(head);
+
+    evenAfterOdd(head);
+
+    display(head);
+
+    return 0;
+}
